e6617fb9c7eaa50643dbd12464ab894f1da5912d
Brian Haubrick <bhaubrick@telus.net>
2020-09-08 09:09:43 -0700

    saving changes to project to get app.get route to work

--- server/server.js
+++ server/server.js
@@ -25,17 +25,17 @@ connection.connect((err) => {
   console.log(`db connected ✅`);
 });
 
-const api = new SpoonacularApi.DefaultApi();
-const q = '';
-const callback = function (error, data, response) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('API called successfully. returned data: ' + data);
-  }
-  console.log(q);
-  api.analyzeARecipeSearchQuery(q, callback);
-};
+// const api = new SpoonacularApi.DefaultApi();
+// const q = '';
+// const callback = function (error, data, response) {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     console.log('API called successfully. returned data: ' + data);
+//   }
+//   console.log(q);
+//   api.analyzeARecipeSearchQuery(q, callback);
+// };
 
 app.get('/', (req, res) => {
   res.json({
@@ -83,8 +83,8 @@ app.get(`/users/:id`, (req, res) => {
   const { id } = req.params;
 
   connection.query(
-    `SELECT * FROM users WHERE user.id = ${id};
-     SELECT * FROM shoppinglists WHERE shoppinglists.user_id = ${id};`,
+    `SELECT * FROM users WHERE users.id = ${Number(id)};
+     SELECT * FROM shoppinglists WHERE shoppinglists.user_id = ${Number(id)};`,
     (err, data) => {
       if (err) throw err;
       if (data[0].length && data[1].length) {
@@ -94,6 +94,59 @@ app.get(`/users/:id`, (req, res) => {
       }
     }
   );
+});
+
+app.post('/users/', (req, res) => {
+  const { name, address, phone, email, credit_card, expiry_date } = req.body;
+  console.log(req.body);
+  if (name && address && phone && email && credit_card && expiry_date) {
+    connection.query(
+      `INSERT INTO users
+      (name, address, phone, email, credit_card, expiry_date) VALUES (
+        '${name}','${address}','${phone}','${email}','${credit_card}','${expiry_date}');`,
+      (err, data) => {
+        console.log('data', data.affectedRows);
+        if (err) throw err;
+        if (data) {
+          res.json({ data });
+        } else {
+          res.json({ message: 'no data' });
+        }
+      }
+    );
+  } else {
+    res.status(401).json({
+      message:
+        'please provide a name, address, phone number, email address, credit card, and expiry date',
+    });
+  }
+});
+
+app.post('/shoppinglists/', (req, res) => {
+  const { description, price, quantity, flyer_item, user_id } = req.body;
+  console.log(req.body);
+  if (description && price && quantity && flyer_item && user_id) {
+    connection.query(
+      `INSERT INTO shoppinglists
+      (description, price, quantity, flyer_item, user_id) VALUES (
+        '${description}','${price}','${quantity}','${flyer_item}','${user_id}'
+      );`,
+      (err, data) => {
+        console.log('data', data.affectedRows);
+        if (err) throw err;
+        if (data) {
+          res.json({ data });
+        } else {
+          res.json({ message: 'no data' });
+        }
+      }
+    );
+  } else {
+    res.status(401).json({
+      message:
+        'please privide a description, price, quantity, flyer_item, and user_id',
+    });
+  }
 });
 
 app.listen(5000, console.log(`http://localhost:5000`));
